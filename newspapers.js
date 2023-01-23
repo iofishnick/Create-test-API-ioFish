3d5d349fe90378b1daf8b3f6a6aa4a148b6b4ba9
YiannisPsar <yiannis.psaronikolakis@satorianalytics.com>
2023-01-23 14:50:33 +0200

    Init

---
+++ newspapers.js
@@ -0,0 +1,19 @@
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: '',
+  },
+  {
+    name: 'theguardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: '',
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+    base: 'https://www.telegraph.co.uk',
+  },
+];
+
+module.exports = { newspapers };
